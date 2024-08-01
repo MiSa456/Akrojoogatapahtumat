536189ce4f9aef5675037739418c3f0c73605e89
Mira Salonen <p33155@edu.sasky.fi>
2024-08-01 16:12:15 +0300

    käyttäjätietojen hakeminen

--- public/index.php
+++ public/index.php
@@ -5,6 +5,14 @@ session_start();
 
 // Suoritetaan projektin alustusskripti.
 require_once '../src/init.php';
+  
+// Haetaan kirjautuneen käyttäjän tiedot.
+if (isset($_SESSION['user'])) {
+  require_once MODEL_DIR . 'henkilo.php';
+  $loggeduser = haeHenkilo($_SESSION['user']);
+} else {
+  $loggeduser = NULL;
+}
 
 // Siistitään polku urlin alusta ja mahdolliset parametrit urlin lopusta.
 // Siistimisen jälkeen osoite /~msalonen/acroyoga/tapahtuma?id=1 on 
