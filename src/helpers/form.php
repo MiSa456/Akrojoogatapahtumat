fe2858e924c403e5af73b5a4d1cc78641aa24125
Mira Salonen <p33155@edu.sasky.fi>
2024-08-01 10:34:23 +0300

    lomaketiedon siistiminen

---
+++ src/helpers/form.php
@@ -0,0 +1,12 @@
+<?php
+function cleanArrayData($array=[]) {
+  $result = array();
+  foreach ($array as $key => $value) {
+    $cleaned = trim($value);
+    $cleaned = stripslashes($cleaned);
+    $result[$key] = $cleaned;
+  }
+  return $result;
+}
+
+?>
