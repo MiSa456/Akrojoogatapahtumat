b6c1472b1416e106b99b3eaa21729e5134349971
Mira Salonen <p33155@edu.sasky.fi>
2024-08-02 09:38:12 +0300

    ilmoittaudu-nappi tapahtumasivulle

--- src/view/tapahtuma.php
+++ src/view/tapahtuma.php
@@ -7,3 +7,17 @@
 <h1><?=$tapahtuma['nimi']?></h1>
 <div><?=$tapahtuma['kuvaus']?></div>
 <div>Ajankohta: <?=$start->format('j.n.Y')?> klo <?=$start->format('G:i')?></div>
+
+<?php
+  if ($loggeduser) {
+    if (!$ilmoittautuminen) {
+      echo "<div class='flexarea'><a href='ilmoittaudu?id=$tapahtuma[idtapahtuma]' class='button'>ILMOITTAUDU</a></div>";    
+    } else {
+      echo "<div class='flexarea'>";
+      echo "<div>Olet ilmoittautunut tapahtumaan!</div>";
+      echo "<a href='peru?id=$tapahtuma[idtapahtuma]' class='button'>PERU ILMOITTAUTUMINEN</a>";
+      echo "</div>";
+    }
+  }
+
+?>
